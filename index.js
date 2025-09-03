d7924056da9456fcffb75d377000c84486e462e8
Seren Morvek <229083256+serenmorvek@users.noreply.github.com>
2025-09-03 10:32:30 +0300

    exercise 3.7

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 let persons = [
@@ -25,6 +27,7 @@ let persons = [
 ]
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 app.get('/api/persons', (request, response) => {
     response.json(persons)
