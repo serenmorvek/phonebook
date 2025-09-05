2603679327124a9e8fdebd95dcb47f928cb1cd86
Seren Morvek <229083256+serenmorvek@users.noreply.github.com>
2025-09-05 13:34:13 +0300

    exercise 3.20

--- index.js
+++ index.js
@@ -49,11 +49,11 @@ app.get('/api/persons/:id', (request, response, next) => {
 app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
-    if (!body.name || !body.number) {
-        return response.status(400).json({
-            error: 'name or number missing',
-        })
-    }
+    // if (!body.name || !body.number) {
+    //     return response.status(400).json({
+    //         error: 'name or number missing',
+    //     })
+    // }
     
     const person = new Person({
         name: body.name,
